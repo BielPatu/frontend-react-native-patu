bbb56ef8606f006f1147cc69113bca3af595feb4
BielPatu <gabrielpatu739@gmail.com>
2025-09-11 16:27:23 -0300

    feat:telas placeholder

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,79 +1,58 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Button, Platform, StyleSheet, View } from 'react-native';
 
 import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { Link } from 'expo-router';
+import { Text, TextInput } from 'react-native-gesture-handler';
+import { useEffect, useState } from 'react';
 
 export default function HomeScreen() {
+  const[nome, setNome] = useState(' ');
+  const[email, setEmail] = useState(' ');
+
+
+  function handleSubmit() 
+  {
+    console.log(nome);
+    console.log(email);
+  }  
+
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={'https://as1.ftcdn.net/jpg/03/16/06/18/1000_F_316061887_nCWfeJKqbIxgpaAkgce2lhAfXnAE06Te.jpg'}
           style={styles.reactLogo}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+      <View style={styles.container}>
+      <ThemedText style={styles.semiTitle} >Nome</ThemedText>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your name"
+        value={nome}
+        onChangeText={setNome}
+      />
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      <ThemedText style={styles.semiTitle}>Email</ThemedText>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+      />
+
+      <Button title="Entrar" onPress={handleSubmit}/>
+    </View>
+
+
+      
     </ParallaxScrollView>
   );
 }
@@ -84,15 +63,46 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     gap: 8,
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
+    height: '100%',
+    width: '100%',
     bottom: 0,
     left: 0,
     position: 'absolute',
+    
   },
+  input: 
+  {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 20,
+    border: '1px solid #000',
+    borderRadius: '5px',
+    padding: '5px',
+    height: '100%',
+    width: '100%'
+  },
+  container: 
+  {
+    display: 'flex',
+    alignItems: 'center',
+    margin: 8,
+    padding: 10,
+    boxShadow: '0px 0px 10px #5a5a5aff',
+    borderRadius: '5px'
+  },
+  semiTitle: 
+  {
+    fontWeight: 'bold'
+  }
+  
 });
